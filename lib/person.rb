440f3f3aa32173f90b2fed07d6d42d545144c5ae
Yining Wang <yining.wang@utexas.edu>
2019-10-30 15:53:23 -0500

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,86 @@
-# your code goes here
+class Person
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def name
+        @name
+    end
+
+    def bank_account
+        @bank_account
+    end
+
+    def bank_account=(account)
+        @bank_account = account
+    end
+
+    def happiness
+        @happiness
+    end
+
+    def happiness=(happiness)
+        @happiness = happiness
+        @happiness = 10 if @happiness > 10
+        @happiness = 0 if @happiness < 0
+    end
+
+    def hygiene
+        @hygiene
+    end
+
+    def hygiene=(hygiene)
+        @hygiene = hygiene
+        @hygiene = 10 if @hygiene > 10
+        @hygiene = 0 if @hygiene < 0
+    end
+
+    def clean?
+        @hygiene > 7
+    end
+    
+    def happy?
+        @happiness > 7
+    end
+
+    def get_paid(amount)
+        @bank_account += amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(chat_friend, topic)
+        if topic == "politics"
+            self.happiness -= 2
+            chat_friend.happiness -= 2
+            "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness += 1
+            chat_friend.happiness += 1
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+    end
+end
+    
